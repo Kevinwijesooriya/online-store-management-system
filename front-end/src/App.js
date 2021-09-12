278211b42de9a5d6cd20e382dd927a024f4611ba
kevinwijesooriya <kevinwijesooriya@gmail.com>
2021-09-12 11:38:00 +0530

    front end updated

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router} from 'react-router-dom'
+import SalaryPlanApp from './components/SalaryPlanApp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className = "App">
+      <SalaryPlanApp />     
+    
     </div>
+    </Router>
+
   );
 }
 
